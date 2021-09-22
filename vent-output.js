366ecde6b1e87241ac493b5ad6cf1110086b52ef
rajaniraju <rajaniraju2003@gmail.com>
2021-09-22 17:23:33 -0500

    Correcting the Layout

--- vent-output.js
+++ vent-output.js
@@ -1,15 +1,15 @@
 class VentOutput extends HTMLElement {
-	// Specify observed attributes so that
-	// attributeChangedCallback will work
-	static get observedAttributes() {
-		return ["v", "l", "u"];
-	}
+  // Specify observed attributes so that
+  // attributeChangedCallback will work
+  static get observedAttributes() {
+    return ["v", "l", "u"];
+  }
 
-	constructor() {
-		super();
-		this._ventbutton;
-		this.attachShadow({ mode: "open" });
-		this.shadowRoot.innerHTML = `
+  constructor() {
+    super();
+    this._ventbutton;
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
 		<style>
 		.flex-container {
 			display: flex;
@@ -85,30 +85,27 @@ class VentOutput extends HTMLElement {
 					</div>
 				</div>	
 
-				<div class ="container">
-					<div><input class="limit" id="max_h" type='text' value="48"> </input></div>
-					<div><input class="limit" id="max_l" type='text'value="43"> </input></div>
-				</div>
+				
 			</div>  	
 		</div>		
 		`;
-	}
+  }
 
-	connectedCallback() {
-		this._ventbutton = this.shadowRoot.querySelector(".vent-output");
-	}
+  connectedCallback() {
+    this._ventbutton = this.shadowRoot.querySelector(".vent-output");
+  }
 
-	diconnectedCallback() { }
+  diconnectedCallback() {}
 
-	attributeChangedCallback(name, oldValue, newValue) {
-		if (name == "v") {
-			this.shadowRoot.querySelector(".v").innerHTML = newValue;
-			this._currentVal = newValue;
-		} else if (name == "l") {
-			this.shadowRoot.querySelector(".l").innerHTML = newValue;
-		} else if (name == "u") {
-			this.shadowRoot.querySelector(".u").innerHTML = newValue;
-		}
-	}
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name == "v") {
+      this.shadowRoot.querySelector(".v").innerHTML = newValue;
+      this._currentVal = newValue;
+    } else if (name == "l") {
+      this.shadowRoot.querySelector(".l").innerHTML = newValue;
+    } else if (name == "u") {
+      this.shadowRoot.querySelector(".u").innerHTML = newValue;
+    }
+  }
 }
 customElements.define("vent-output", VentOutput);
